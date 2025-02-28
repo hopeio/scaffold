91b1f33615c588360f59fe1a1db40212182e1d10
贾一饼 <Boyang.Liang@apulis.com>
2025-02-28 15:55:40 +0800

    fix: jwt

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -2,21 +2,21 @@ package jwt
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/hopeio/context/reqctx"
 	stringsi "github.com/hopeio/utils/strings"
 	jwti "github.com/hopeio/utils/validation/auth/jwt"
+	"reflect"
 )
 
 type authorization[A reqctx.AuthInfo] struct {
-	Authorization[A]
+	jwti.Claims[A]
 	AuthInfoRaw string `json:"-"`
 }
 
-type Authorization[A reqctx.AuthInfo] = jwti.Claims[A]
-
 func (x *authorization[A]) UnmarshalJSON(data []byte) error {
 	x.AuthInfoRaw = stringsi.BytesToString(data)
-	return json.Unmarshal(data, &x.Authorization)
+	return json.Unmarshal(data, &x.Claims)
 }
 
 func (x *authorization[A]) ParseToken(token string, secret []byte) error {
@@ -24,18 +24,24 @@ func (x *authorization[A]) ParseToken(token string, secret []byte) error {
 	if err != nil {
 		return err
 	}
-	x.ID = x.Data.IdStr()
+	rv := reflect.ValueOf(x.Auth)
+	if rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			return errors.New("auth info is nil")
+		}
+	}
+	x.ID = x.Auth.IdStr()
 	return nil
 }
 
-func Auth[REQ reqctx.ReqCtx, A reqctx.AuthInfo](ctx *reqctx.Context[REQ], secret []byte) (*Authorization[A], error) {
+func Auth[REQ reqctx.ReqCtx, A reqctx.AuthInfo](ctx *reqctx.Context[REQ], secret []byte) (*jwti.Claims[A], error) {
 	authorization := authorization[A]{}
 	if err := authorization.ParseToken(ctx.Token, secret); err != nil {
 		return nil, err
 	}
-	authInfo := authorization.Data
+	authInfo := authorization.Auth
 	ctx.AuthID = authorization.ID
 	ctx.AuthInfo = authInfo
 	ctx.AuthInfoRaw = authorization.AuthInfoRaw
-	return &authorization.Authorization, nil
+	return &authorization.Claims, nil
 }
