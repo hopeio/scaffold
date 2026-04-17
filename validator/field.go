f260c416fe2c2a756d755565a48c7894bf97f53f
贾一饼 <lby.i@qq.com>
2026-04-17 19:23:34 +0800

    feat: validator

---
+++ validator/field.go
@@ -0,0 +1,31 @@
+package validator
+
+import "regexp"
+
+const (
+	Phone = iota
+	Mail
+	Unknown
+)
+const (
+	emailPattern = `^[a-zA-Z0-9.!#$%&'*+/=?^_` + "`" + `{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`
+	phonePattern = `^1[0-9]{10}$`
+)
+
+func PhoneOrMail(input string) int {
+	phoneMatch, _ := regexp.MatchString(phonePattern, input)
+	if phoneMatch {
+		return Phone
+	} else {
+		emailMatch, _ := regexp.MatchString(emailPattern, input)
+		if emailMatch {
+			return Mail
+		}
+	}
+	return Unknown
+}
+
+func IsPhone(input string) bool {
+	phoneMatch, _ := regexp.MatchString(phonePattern, input)
+	return phoneMatch
+}
